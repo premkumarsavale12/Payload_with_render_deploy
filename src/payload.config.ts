b96a315773b43c0eadd6f6bca220275addbaf026
Premkumar Savale <premsavle112@gmail.com>
2025-12-23 15:47:56 +0530

    fix: enable db schema push to resolve missing tables error

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -74,6 +74,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL,
     },
+    push: true,
   }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
