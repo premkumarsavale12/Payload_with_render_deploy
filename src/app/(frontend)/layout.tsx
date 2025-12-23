82f797a844228da93171e6ecc54006c3c9949807
Premkumar Savale <premsavle112@gmail.com>
2025-12-23 12:54:46 +0530

    Added Code

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -6,7 +6,7 @@ import { GeistSans } from 'geist/font/sans'
 import React from 'react'
 
 import { AdminBar } from '@/components/AdminBar'
-import { Footer } from '@/Footer/Component'
+// import { Footer } from '@/Footer/Component'
 import { Header } from '@/Header/Component'
 import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
@@ -36,7 +36,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
           <Header />
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </Providers>
       </body>
     </html>
